a8f905d3bb995dd3adc197c39e67eaaaa867c2a0
kidzas <kidza883@gmail.com>
2018-01-17 15:33:41 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,6 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'qKPUpoabrPGOzlbGXaOkyNHDA6RzQXLk1qD/1An/AJkB6sKwgY79pb/lY6at9komRuJHRO49zndSnToj045CWh6H/O464wZTU2Sp078bIbwyf5K5xME0SFjfc92bmcGxl+f7R9mk8B11B25WKiS6JQdB04t89/1O/w1cDnyilFU=
+';
 
 // Get POST body content
 $content = file_get_contents('php://input');
